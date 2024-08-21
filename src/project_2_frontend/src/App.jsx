b58e8123a03b3c078720036d5a00ebc32b758cbf
dafajudin <dafajudin@365.telkomuniversity.ac.id>
2024-08-21 21:37:46 +0700

    first project

--- src/project_2_frontend/src/App.jsx
+++ src/project_2_frontend/src/App.jsx
@@ -1,29 +1,39 @@
 import { useState } from 'react';
-import { project_2_backend } from 'declarations/project_2_backend';
+import { hacker_house_challenge_2_backend } from 'declarations/hacker-house-challenge-2-backend';
 
 function App() {
-  const [greeting, setGreeting] = useState('');
+  const [studentName, setStudentName] = useState('');
+  const [students, setStudents] = useState([]);
 
   function handleSubmit(event) {
     event.preventDefault();
-    const name = event.target.elements.name.value;
-    project_2_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
+    hacker_house_challenge_2_backend.get_student_data(studentName).then((response) => {
+      const parsed = JSON.parse(response);
+      console.log(parsed);
+      setStudents(parsed.mahasiswa);
     });
     return false;
   }
 
   return (
     <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
+      <h1>Student Info Finder</h1>
+      <div>
+        <h1>Find Student Info by Name</h1>
+        <h5>Student Name:</h5>
+        <input type="text" onChange={(ev) => setStudentName(ev.target.value)} />
+        <button onClick={handleSubmit}>Find Student Info</button>
+      </div>
+      <div>
+        <h2>Results:</h2>
+        <ul>
+          {students.map((student, index) => (
+            <li key={index}>
+              {student.text} - <a href={`https://api-frontend.kemdikbud.go.id${student["website-link"]}`} target="_blank" rel="noopener noreferrer">Link</a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </main>
   );
 }
